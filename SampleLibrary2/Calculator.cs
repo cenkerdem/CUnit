7c076593d3f8f9252abe9f886ad0b474cf54f4b9
cenk <cenk@cenk>
2019-06-11 00:36:11 +0300

    CUnit

---
+++ SampleLibrary2/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using SampleMathLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLibrary2
+{
+    class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return SampleMath.Sum(a, b);
+        }
+
+        public int Substract(int a, int b)
+        {
+            return SampleMath.Substract(a, b);
+        }
+    }
+}
---
+++ SampleMathLibrary/SampleMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMathLibrary
+{
+    public static class SampleMath
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
