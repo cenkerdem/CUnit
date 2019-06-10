bce7cd305317d3c753a9793d32506616c747b7a7
cenk <cenk@cenk>
2019-06-11 01:07:22 +0300

    Generic assertion

--- CUnit/Assert.cs
+++ CUnit/Assert.cs
@@ -9,29 +9,30 @@ namespace CUnit
 {
     public static class Assert
     {
-        public static void Equals(object first, object second) {
-            if (!first.Equals(second))
-            {
-                List<object> parameters = new List<object>() { first, second };
-                throw new AssertionException("Objects Are Not Equal", AssertionType.EqualityCheck, parameters);
-            }
+        public static void Equals(object target, object expected) {
+            Equals<object>(target, expected);
         }
 
-        public static void Equals(long first, long second)
+        public static void Equals(long target, long expected)
         {
-            if (!first.Equals(second))
-            {
-                List<object> parameters = new List<object>() { first, second };
-                throw new AssertionException(string.Format("Numbers Are Not Equal. Values: {0}, {1}", first, second), AssertionType.EqualityCheck, parameters);
-            }
+            Equals<long>(target, expected, EqualityComparer<long>.Default, "Numbers Are Not Equal. Values: {0}, {1}");
         }
 
-        public static void Equals(int first, int second)
+        public static void Equals(int target, int expected)
         {
-            if (!first.Equals(second))
-            {
-                List<object> parameters = new List<object>() { first, second };
-                throw new AssertionException(string.Format("Numbers Are Not Equal. Values: {0}, {1}", first, second), AssertionType.EqualityCheck, parameters);
+            Equals<int>(target, expected, EqualityComparer<int>.Default, "Numbers Are Not Equal. Values: {0}, {1}");
+        }
+
+        public static void Equals<T>(T target, T expected, string message = "Values Are Not Equal. Values: {0}, {1}")
+        {
+            Equals<T>(target, expected, EqualityComparer<T>.Default, "Numbers Are Not Equal. Values: {0}, {1}");
+        }
+
+        public static void Equals<T>(T target, T expected, IEqualityComparer<T> comparer, string message = "Values Are Not Equal. Values: {0}, {1}")
+        {
+            List<object> parameters = new List<object>() { target, expected };
+            if(!comparer.Equals(target, expected)) {
+                throw new AssertionException(string.Format(message, target, expected), AssertionType.EqualityCheck, parameters);
             }
         }
     }
