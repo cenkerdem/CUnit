d3696b8441c8847f71c3ee4ee01fb5c1603de678
cenk <cenk@cenk>
2019-06-11 01:51:54 +0300

    program only uses cunit test

--- CUnit/Program.cs
+++ CUnit/Program.cs
@@ -40,35 +40,6 @@ namespace CUnit
             }
 
             Console.ReadLine();
-            string currentDirectory = Directory.GetCurrentDirectory();
-            return;
-
-            //bool loopContinues = true;
-            //while (loopContinues)
-            //{
-            //    string functionCode = Console.ReadLine();
-                
-            //    switch (functionCode.ToUpperInvariant())
-            //    {
-            //        case "SINGLETON":
-            //            TestSingleton();
-            //            break;
-            //        case "\0":
-            //            loopContinues = false;
-            //            break;
-            //    }
-            //}
         }
-
-        //private static void TestSingleton()
-        //{
-        //    HTMLGenerator generator = SingletonProvider<HTMLGenerator>.Instance;
-        //    Customer testCustomer = new Customer()
-        //    {
-        //        Id = 1,
-        //        Name = "Cenk"
-        //    };
-        //    string generatedHTML = generator.GetHTML(testCustomer);
-        //}
     }
 }
